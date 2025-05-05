fe274b2e1982eefde9297d9459450d0e06a978f4
Zachary White <zacharywhite@Zacharys-MacBook-Pro-10.local>
2025-05-04 20:59:24 -0600

    Black pawn logic

--- shared/src/main/java/chess/PawnMoves.java
+++ shared/src/main/java/chess/PawnMoves.java
@@ -99,7 +99,86 @@ public class PawnMoves {
 
 
         if (movingPawn.getTeamColor().equals(ChessGame.TeamColor.BLACK)){
-
+            y--;
+            currentIteration.setRow(y);
+            ChessPiece potential_move = board.getPiece(currentIteration);
+            if (myPosition.getRow() > 2) {
+                //move forward 1 logic
+                if (potential_move == null) {  //what to do if the potential space the piece could move to is empty
+                    ChessPosition movablePos = new ChessPosition(y, x);  //create copy of currentIteration so copy doesn't update when we don't want it to
+                    movable_places.add(new ChessMove(myPosition, movablePos, null));  //add copy to list of possible moves
+                }
+                //attack right logic
+                x++;
+                if (x<9) {
+                    currentIteration.setColumn(x);
+                    potential_move = board.getPiece(currentIteration);
+                    if (potential_move != null && !potential_move.getTeamColor().equals(movingPawn.getTeamColor())) {
+                        ChessPosition movablePos = new ChessPosition(y, x);  //create copy of currentIteration so copy doesn't update when we don't want it to
+                        movable_places.add(new ChessMove(myPosition, movablePos, null));  //add copy to list of possible moves
+                    }
+                }
+                //attack left logic
+                x = x - 2;
+                if (x>0) {
+                    currentIteration.setColumn(x);
+                    potential_move = board.getPiece(currentIteration);
+                    if (potential_move != null && !potential_move.getTeamColor().equals(movingPawn.getTeamColor())) {
+                        ChessPosition movablePos = new ChessPosition(y, x);  //create copy of currentIteration so copy doesn't update when we don't want it to
+                        movable_places.add(new ChessMove(myPosition, movablePos, null));  //add copy to list of possible moves
+                    }
+                }
+                //move forward 2 if on starting location logic
+                x++;
+                currentIteration.setColumn(x);
+                potential_move = board.getPiece(currentIteration);
+                if (myPosition.getRow() == 7 && potential_move == null) {
+                    y--;
+                    currentIteration.setRow(y);
+                    potential_move = board.getPiece(currentIteration);
+                    if (potential_move == null) {  //what to do if the potential space the piece could move to is empty
+                        ChessPosition movablePos = new ChessPosition(y, x);  //create copy of currentIteration so copy doesn't update when we don't want it to
+                        movable_places.add(new ChessMove(myPosition, movablePos, null));  //add copy to list of possible moves
+                    }
+                }
+            }
+            //promotion logic
+            if (myPosition.getRow() == 2) {
+                //move forward 1 logic
+                if (potential_move == null) {  //what to do if the potential space the piece could move to is empty
+                    ChessPosition movablePos = new ChessPosition(y, x);  //create copy of currentIteration so copy doesn't update when we don't want it to
+                    movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.KNIGHT));
+                    movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.BISHOP));
+                    movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.ROOK));
+                    movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.QUEEN));//add copy to list of possible moves
+                }
+                //attack right logic
+                x++;
+                if (x<9) {
+                    currentIteration.setColumn(x);
+                    potential_move = board.getPiece(currentIteration);
+                    if (potential_move != null && !potential_move.getTeamColor().equals(movingPawn.getTeamColor())) {
+                        ChessPosition movablePos = new ChessPosition(y, x);  //create copy of currentIteration so copy doesn't update when we don't want it to
+                        movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.KNIGHT));
+                        movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.BISHOP));
+                        movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.ROOK));
+                        movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.QUEEN));  //add copy to list of possible moves
+                    }
+                }
+                //attack left logic
+                x = x - 2;
+                if (x > 0) {
+                    currentIteration.setColumn(x);
+                    potential_move = board.getPiece(currentIteration);
+                    if (potential_move != null && !potential_move.getTeamColor().equals(movingPawn.getTeamColor())) {
+                        ChessPosition movablePos = new ChessPosition(y, x);  //create copy of currentIteration so copy doesn't update when we don't want it to
+                        movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.KNIGHT));
+                        movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.BISHOP));
+                        movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.ROOK));
+                        movable_places.add(new ChessMove(myPosition, movablePos, ChessPiece.PieceType.QUEEN));  //add copy to list of possible moves
+                    }
+                }
+            }
         }
         return movable_places;
     }

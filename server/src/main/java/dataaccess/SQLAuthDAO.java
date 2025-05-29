2e445b6e42990713d870d474992072b4276660d6
Zachary White <zacharywhite@zacharys-mbp-10.lan>
2025-05-28 19:28:55 -0600

    SQLAuthDAO methods added

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -17,22 +17,61 @@ public class SQLAuthDAO implements AuthDAO{
 
     @Override
     public void createAuth(AuthData auth) {
+        var sql = "INSERT INTO auths (authToken, username) VALUES (?, ?)";
 
+        try (Connection connection = DatabaseManager.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            // sets values to the placeholders and then updates
+            statement.setString(1, auth.authToken());
+            statement.setString(2, auth.username());
+            statement.executeUpdate();
+        } catch (SQLException | DataAccessException e) {
+            throw new BadRequestException("bad request");
+        }
     }
 
     @Override
     public AuthData getAuth(String authToken) {
-        return null;
+        var sql = "SELECT authToken, username FROM auths WHERE authToken=?";
+        try (Connection connection = DatabaseManager.getConnection()){
+            try (var ps = connection.prepareStatement(sql)) {
+                ps.setString(1, authToken);
+                try (var rs = ps.executeQuery()){
+                    if (rs.next()) {
+                        String username = rs.getString("username");
+                        return new AuthData(authToken,username);
+                    } else {
+                        return null;
+                    }
+                }
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void deleteAuth(AuthData auth) {
-
+        var sql = "DELETE FROM auths WHERE authToken=?";
+        try (Connection connection = DatabaseManager.getConnection()) {
+            try (var statement = connection.prepareStatement(sql)) {
+                statement.setString(1, auth.authToken());
+                statement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void clear() {
-
+        var sql = "TRUNCATE auths";
+        try (Connection connection = DatabaseManager.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.executeUpdate();
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private final String[] createStatements = {

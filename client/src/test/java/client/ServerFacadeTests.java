d1a4e86e1c907163c89b83a2009a5765ca32d947
Zachary White <zacharywhite@Zacharys-MBP-10.localdomain>
2025-06-04 18:38:00 -0600

    debugged

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -15,13 +15,14 @@ import java.util.Collection;
 public class ServerFacadeTests {
 
     private static Server server;
-    private static ServerFacade facade = new ServerFacade("http://localhost:8080");
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(8080);
+        var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
